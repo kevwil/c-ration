f0b476438dbc7f910580eb11f18487e7acfe1843
Kevin Williams <kevwil@gmail.com>
2012-12-10 11:37:35 -0700

    get Fabric deploy script working

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -10,28 +10,42 @@ import os
 # override with '-u <username>' on command-line
 env.user = 'ubuntu'
 
-env.packagename = 'c-ration-1.0-SNAPSHOT'
+dist_package_name = 'c-ration-1.0-SNAPSHOT'
 
 @task
-def deploy(appdir='/opt',config_repo='/opt/config'):
+def deploy(approot='/home/appuser',config_repo='/opt/config'):
   check_remote_setup()
   with settings(hide('warnings'),warn_only=True):
     sudo("stop c-ration")
   update_config(config_repo)
   local("play clean compile dist")
-  put("dist/"+env.packagename+".zip", appdir, use_sudo=True)
-  with cd(appdir):
-    sudo("unzip -l "+env.packagename+".zip")
-  if not exists("/etc/init/c-ration", use_sudo=True)
-    sudo("echo '#!/bin/sh\n\ndescription\t\"configuration server\"\nauthor\t\"Kevin Williams\"\nstart on startup\nexec "+appdir+"/c-ration/start -Dconfigfiles.rootdir="+config_repo+"\n' > /etc/init/c-ration")
+  if exists(approot+"/"+dist_package_name+".zip"):
+    sudo("rm "+approot+"/"+dist_package_name+".zip")
+  appdir=approot+"/"+dist_package_name
+  if exists(appdir):
+    sudo("rm -rf "+appdir)
+  put("dist/"+dist_package_name+".zip", approot+"/"+dist_package_name+".zip", use_sudo=True)
+  sudo("chown appuser:appuser "+appdir+".zip")
+  with cd(approot):
+    sudo("su - appuser -c 'unzip "+dist_package_name+".zip'")
+  template_file=os.path.abspath(os.path.join(os.path.dirname(__file__),"c-ration.conf.template"))
+  upload_template(template_file, "/etc/init/c-ration.conf",{"appdir":appdir,"config_repo":config_repo},use_sudo=True)
+  sudo("chown root:root /etc/init/c-ration.conf")
+  sudo("chmod 755 /etc/init/c-ration.conf")
   sudo("start c-ration")
 
 @task
-def update_config(config_repo)
-  with cd(config_repo):
-    run('git pull')
+def apply_config(config_repo='/opt/config'):
+  update_config(config_repo)
+  sudo("restart c-ration")
   
-def check_remote_setup()
+def update_config(config_repo):
+  if not exists(config_repo):
+    abort("need to create config repo on remote host first")
+  with cd(config_repo):
+    sudo('git pull')
+  
+def check_remote_setup():
   with settings(hide('warnings'),warn_only=True):
     run('test -e `which git`')
     run('test -e `which java`')
